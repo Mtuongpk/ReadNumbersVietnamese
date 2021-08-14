2791b0613fb8cd84d90464758ec45f663a72bc8e
Mtuongpk <mtuongpk@gmail.com>
2021-08-14 11:29:48 +0700

    read numbers to Vietnamese words

---
+++ DocSo.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocSoThanhChu
+{
+    class DocSo
+    {
+        string[] chucai = { "không ", "một ", "hai ", "ba ", "bốn ", "năm ", "sáu ", "bảy ", "tám ", "chín ", "mười " };
+        string[] donvi = { "", "nghìn", "triệu" };
+        public void SoSangChu(int a)
+        {
+            Console.Write(chucai[a]);
+            //In chữ cái trong chuỗi chucai lên màn hình
+        }
+        public void DocDonVi(int a)
+        {
+            Console.Write(donvi[a]);
+        }
+        //Hàm đọc hai chữ số
+        //----------------------------------------------------------------------
+        public void TwoDigit(int a)
+        {
+            int hc, dv; //Biến lưu chữ số hàng chục và đơn vị của số có 2 chữ số
+            hc = a / 10;
+            dv = a % 10;
+            //----------------------------------------
+            //Đọc chữ số hàng chục
+            if (hc == 1)
+            {
+                Console.Write("mười ");
+            }
+            else if (hc != 0)
+            {
+                SoSangChu(hc);
+                Console.Write("mươi ");
+            }
+            else
+            {
+                SoSangChu(hc);
+            }
+            //----------------------------------------
+            //Đọc chữ số hàng đơn vị
+            if (dv != 1 && dv != 4 && dv != 5)
+            {
+                //expect 16: mười sáu
+                //69: sáu mươi chín
+                if (dv != 0)
+                {
+                    SoSangChu(dv);
+                }
+            }
+            if (dv == 1)
+            {
+                //expect 11: mười một
+                //21: hai mươi mốt
+                if (hc == 1 || hc == 0)
+                {
+                    SoSangChu(dv);
+                }
+                else
+                    Console.Write("mốt ");
+            }
+            if (dv == 4)
+            {
+                //expect 14: mười bốn
+                //24: hai mươi tư
+                if (hc == 1 || hc == 0)
+                    SoSangChu(dv);
+                else
+                    Console.Write("tư ");
+            }
+            if (dv == 5)
+            {
+                //expect 15: mười lăm
+                //05: năm (305: ba trăm linh năm)
+                if (hc != 0)
+                    Console.Write("lăm ");
+                else
+                    SoSangChu(dv);
+            }
+        }
+        //Hàm đọc ba chữ số
+        //----------------------------------------------------------------------
+        public void ThreeDigit(int a, int b)
+        {
+            int ht, hc, dv;//Biến lưu trữ hàng trăm, hàng chục, đơn vị của số có 3 chữ số
+            ht = a / 100;
+            dv = a % 10;
+            hc = (a / 10) % 10;
+            //b: variable store a signal.
+            //expect b == 1: 028 đọc là không trăm hai mươi tám
+            // b == 0: 028 đọc là hai mươi tám
+            if (b == 1)
+            {
+                if (ht == 0)
+                {
+                    //hàng trăm có giá trị 0
+                    if (hc == 0)
+                    {
+                        //Hàng chục có giá trị 0
+                        if (dv == 0)
+                            Console.Write("không ");
+                        else
+                        {
+                            Console.Write("không trăm linh ");
+                            SoSangChu(dv);
+                        }
+                    }
+                    else
+                    {
+                        //hàng chục có giá trị khác không
+                        Console.Write("không trăm ");
+                        TwoDigit(a % 100); //đọc 2 chữ số
+                    }
+                }
+                else
+                    ThreeDigit(a, 0); //gọi lại hàm này với tham số b = 0 trong trường hợp hàng trăm khác 0
+            }
+            else
+            {
+                if (ht != 0)
+                {
+                    SoSangChu(ht);
+                    Console.Write("trăm ");
+                }
+                if (hc == 0)
+                    if (dv == 0)
+                    {
+                        //Console.Write("không ");
+                        return;
+                    }
+                    else
+                    {
+                        if (ht > 0)
+                            Console.Write("linh ");
+                        SoSangChu(dv);
+                    }
+                else
+                    TwoDigit(a % 100);
+            }
+        }
+        //Hàm đọc chín chữ số
+        //----------------------------------------------------------------------
+        public void NineDigit(int n, int k)
+        {
+            int[] a = new int[3]; //Mảng lưu giá trị 3 chữ số, tách ra từ số có 9 chữ số
+            int d = 0;//number of elements of array a
+            if (n < 1000)
+            {
+                ThreeDigit(n, k);
+                return;
+            }
+            else while (n != 0)
+                {
+                    a[d] = n % 1000;
+                    d++;
+                    n /= 1000;
+                }
+            for (var i = d - 1; i >= 0; i--)
+            {
+                ThreeDigit(a[i], k);
+                if ((i > 0 && a[i] != 0))
+                {
+                    DocDonVi(i);
+                    Console.Write(" ");
+                }
+                if (a[i] == 0 && i > 0 && a[i-1] > 0)
+                {
+                    //expect: 1000148 đọc là một triệu không nghìn một trăm bốn mươi tám
+                    Console.Write("không ");
+                    DocDonVi(i);
+                    Console.Write(" ");
+                }
+                if (i > 0 && a[i - 1] != 0)
+                    k = 1;
+                if ((i > 0 && a[i - 1] == 0) || (i == 0 && a[0] == 0))
+                    k = 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocSoThanhChu
+{
+    class Program
+    {
+        static public int SoChuSo(int a)
+        {
+            //Hàm đếm số chữ số của 1 số
+            int d = 0;
+            while (a != 0)
+            {
+                a /= 10;
+                d++;
+            }
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; //Support display vietnamese language
+            Console.SetWindowSize(Console.WindowWidth + 35, Console.WindowHeight);
+            //Set width and height of console screen
+            var cs = new DocSo();
+            int d; //number of elements of array a
+            //for (var i = 0; i < 1000; i++)
+            //{
+            //    cs.ThreeDigit(i, 0);
+            //    Console.WriteLine();
+            //}
+            string chuso; //string chuso dùng để lưu số cần đọc. Tránh bị tràn giá trị do giới hạn kiểu nguyên / thực
+            continueProgram:
+            double[] a = new double[30]; //mảng dùng để tách số cần đọc thành những số có 9 chữ số
+            d = 0;
+            Console.Write("Nhập một số tự nhiên bất kỳ: ");
+            chuso = Console.ReadLine();
+            var len_cs = chuso.Length; // variable store length of chuso
+            //Kiểm tra chuỗi chuso có ký tự nào ngoài số không.
+            foreach (char value in chuso)
+            {
+                if (value < '0' || value > '9')
+                {
+                    Console.WriteLine("Chỉ được nhập số tự nhiên!");
+                    goto checkContinue;
+                }
+            }
+            Console.Write($"Số {chuso} đọc là: ");
+            for (int i = len_cs - 1; i >= 0; i--)
+            {
+                a[d] += (chuso[i] - '0') * Math.Pow(10, (len_cs - i - 1 - (9 * d)));
+                if ((len_cs - i) % 9 == 0)
+                    d++;
+            }
+
+            //for (var i = 0; i <= d; i++)
+            //{
+            //    Console.WriteLine($"{d}, {i}");
+            //    Console.WriteLine(SoChuSo(Convert.ToInt32(a[i])));
+            //    Console.WriteLine(a[i]);
+            //}
+
+            if (a[d] == 0)
+            {
+                 for (int i = d - 1; i >= 0; i--)
+                 {
+                     if ((a[i] > 0 && a[i + 1] == 0) || a[0] > 0)
+                         goto normalCase;
+                 }
+                Console.Write("không ");
+            }
+            normalCase:
+            if (a[d] > 0)
+            {
+                cs.NineDigit(Convert.ToInt32(a[d]), 0);
+                for (int i = d; i > 0; i--)
+                    Console.Write("tỷ ");
+            }
+            for (int i = d - 1; i >= 0; i--)
+            {
+                int k = 1;
+                if (a[i] == 0 || (SoChuSo(Convert.ToInt32(a[i])) % 3 != 0 && i != 0) || a[i + 1] == 0)
+                    k = 0;
+                cs.NineDigit(Convert.ToInt32(a[i]), k);
+                for (int j = i; j > 0; j--)
+                {
+                    if (a[j] != 0)
+                        Console.Write("tỷ ");
+                }
+            }
+            checkContinue:
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bạn có muốn tiếp tục không (y/n)?");
+                char kiemtra = char.Parse(Console.ReadLine());
+                if (kiemtra == 'y' || kiemtra == 'Y')
+                {
+                    goto continueProgram;
+                }
+                else
+                    return; // Đóng chương trình
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                Console.Read();
+                return;
+            }
+        }
+    }
+}
